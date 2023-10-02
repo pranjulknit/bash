7a75cffa58f121ccf9586a4d63f6b444a5960f75
Pranjul Chaurasiya <pranjul.19641@knit.ac.in>
2023-10-02 15:29:27 +0530

    for_loop

---
+++ forloop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+	echo "number is $i"
+done
+
+
+
+for name in Raju Shyam Pranjul
+
+do  echo "name in $name"
+
+done
+
+for p in {1..20}
+do	
+	echo "i is $p"
+
+done

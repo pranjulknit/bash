47ca43f6d3e3f50a478938ff6d1b461b1a3d5311
Pranjul Chaurasiya <pranjul.19641@knit.ac.in>
2023-10-02 16:08:57 +0530

    while_loop_in_file

--- for_loop_file.sh
+++ for_loop_file.sh
@@ -8,3 +8,12 @@ for name in $(cat $FILE)
 do
 	echo "Name is $name"
 done
+
+
+myArray=( 1 2 3 Hello Hi )
+length=${#myArray[*]}
+
+for (( i=0; i<$length;i++ ))
+do
+	echo "Value of array is ${myArray[$i]}"
+done
